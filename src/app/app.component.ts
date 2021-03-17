4b3f9ba690111cbc943e65b135f52e3c24dced05
vijay gohel <gohil5898@gmail.com>
2021-03-17 11:53:40 +0530

    get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -23,6 +26,28 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
+  }
+
+  private fetchPosts()
+  {
+    this.http
+    .get("https://angular-http-1fd8a-default-rtdb.firebaseio.com/post.json")
+    .pipe(
+      map(resopnseData=>{
+        const postArray=[];
+        for(const key in resopnseData)
+        {
+          if(resopnseData.hasOwnProperty(key)){
+            postArray.push({...postArray[key],id:key})
+          }
+        }
+      }
+
+      )
+    )
+    .subscribe(respose=>
+      console.log(respose));
   }
 
   onClearPosts() {
